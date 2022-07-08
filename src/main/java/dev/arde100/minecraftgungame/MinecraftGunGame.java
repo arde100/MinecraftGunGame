86cc0e5416930ae29e2e55c1deca88ce4b9bc5a4
arde100 <arde7230@gmail.com>
2022-07-09 00:05:16 +0300

    Inital files

---
+++ src/main/java/dev/arde100/minecraftgungame/MinecraftGunGame.java
@@ -0,0 +1,17 @@
+package dev.arde100.minecraftgungame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftGunGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
