bfeab91ca54c10f31a4938514a7c2b47ca340e6c
BuildTools <unconfigured@null.spigotmc.org>
2022-07-16 02:37:59 +0300

    First somewhat working prototype

--- src/main/java/dev/arde100/minecraftgungame/MinecraftGunGame.java
+++ src/main/java/dev/arde100/minecraftgungame/MinecraftGunGame.java
@@ -1,14 +1,20 @@
 package dev.arde100.minecraftgungame;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,16 +25,16 @@ import java.util.HashMap;
 public final class MinecraftGunGame extends JavaPlugin implements Listener {
 
     public HashMap<Player, Integer> killCounts = new HashMap<>();
-    public HashMap<Integer, ItemStack[]> killItems = new HashMap<>();
-    FileConfiguration config = getConfig();
-    ArrayList<ItemStack> upgradeList = new ArrayList<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
         killCounts.clear();
-        createItems();
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        // Enchantment.getByKey(NamespacedKey.minecraft(""));
+        // Material.valueOf("");
     }
 
     @Override
@@ -36,15 +42,17 @@ public final class MinecraftGunGame extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
-    void updateEquipment() {
-        for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-            // Do stuff
-        }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        killCounts.put(event.getPlayer(), 0);
+        event.getPlayer().getInventory().clear();
+        updateEquipment();
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         if(killCounts.containsKey(event.getPlayer())) {
+            event.getPlayer().getInventory().clear();
             killCounts.remove(event.getPlayer());
         }
     }
@@ -53,64 +61,86 @@ public final class MinecraftGunGame extends JavaPlugin implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getPlayer();
         Player killer = event.getPlayer().getKiller();
-        if(killer instanceof Player) {
+        if(killer.getType() == EntityType.PLAYER) {
             if(killCounts.containsKey(killer)) {
                 killCounts.put(killer, killCounts.get(killer)+1);
             } else {
-                killCounts.put(killer,0);
+                killCounts.put(killer,1);
             }
+            killer.sendMessage(ChatColor.BLUE + "You now have " + killCounts.get(killer) + " kills");
         }
         if(killCounts.containsKey(player)) {
-            killCounts.put(player, Math.round(killCounts.get(player)/2));
+            killCounts.put(player, (int) Math.round((killCounts.get(player)) * (getConfig().getDouble("multiplier-after-death"))));
         } else {
             killCounts.put(player, 0);
         }
+        event.getDrops().clear();
         updateEquipment();
     }
 
-    void createItems() {
-        createWeaponsList();
+    @EventHandler
+    public void onInventoryEvent(InventoryClickEvent event) {
+        event.setCancelled(true);
     }
 
-    void createWeaponsList() {
-        upgradeList.add(new ItemStack(Material.WOODEN_AXE)); // 0
-        upgradeList.add(new ItemStack(Material.WOODEN_SWORD)); // 1
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        updateEquipment();
+    }
 
-        upgradeList.add(new ItemStack(Material.WOODEN_AXE)); // 2
-        upgradeList.add(new ItemStack(Material.WOODEN_SWORD)); // 3
+    void updateEquipment() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Integer kills = killCounts.get(player);
 
-        upgradeList.add(new ItemStack(Material.STONE_AXE)); // 4
-        upgradeList.add(new ItemStack(Material.STONE_SWORD)); // 5
+            if (!(getConfig().contains("killItems." + kills.toString()))) {
+                continue;
+            }
+            Material material = Material.valueOf(getConfig().getString("killItems." + kills + ".item","IRON_SWORD").toUpperCase());
+            ItemStack item = new ItemStack(material);
+            if (!(material == Material.AIR)) {
+                ItemMeta meta = item.getItemMeta();
+                meta.setUnbreakable(true);
+                item.setItemMeta(meta);
+            }
+            if(getConfig().contains("killItems." + kills.toString() + ".enchants")) {
+                for (String key : getConfig().getStringList("killItems." + kills + ".enchants")) {
+                    ItemMeta enchantMeta = item.getItemMeta();
+                    Integer level = Integer.valueOf(key.substring(key.indexOf(" ") + 1));
+                    String enchant = key.substring(0, key.indexOf(" "));
+                    player.sendMessage("" + enchant);
+                    player.sendMessage("" + level);
+                    player.sendMessage("" + Enchantment.getByKey(NamespacedKey.minecraft(enchant.toLowerCase())));
+                    enchantMeta.addEnchant(Enchantment.getByKey(NamespacedKey.minecraft(enchant.toLowerCase())), level, true);
+                    item.setItemMeta(enchantMeta);
+                }
 
-        upgradeList.add(new ItemStack(Material.STONE_AXE)); // 6
-        upgradeList.add(new ItemStack(Material.STONE_SWORD)); // 7
-
-        upgradeList.add(new ItemStack(Material.IRON_AXE)); // 8
-        upgradeList.add(new ItemStack(Material.IRON_SWORD)); // 9
-
-        upgradeList.add(new ItemStack(Material.IRON_AXE)); // 10
-        upgradeList.add(new ItemStack(Material.IRON_SWORD)); // 11
-
-        upgradeList.add(new ItemStack(Material.DIAMOND_AXE)); // 12
-        upgradeList.add(new ItemStack(Material.DIAMOND_SWORD)); // 13
-
-        upgradeList.add(new ItemStack(Material.DIAMOND_AXE)); // 14
-        upgradeList.add(new ItemStack(Material.DIAMOND_SWORD)); // 15
-        // 2, 3, 6, 7, 10, 11, 14, 15
-
-        for (ItemStack weapon : upgradeList) {
-            ItemMeta meta = weapon.getItemMeta();
-            meta.setUnbreakable(true);
-            weapon.setItemMeta(meta);
+            }
+            String mode = getConfig().getString("killItems." + kills + ".mode", "set");
+            Integer slot = getConfig().getInt("killItems." + kills + ".slot", 0);
+            if (mode.equalsIgnoreCase("set")) {
+                if(slot == 100 || slot == 101 || slot == 102 || slot == 103) {
+                    switch (slot) {
+                        case 100:
+                            player.getInventory().setBoots(item);
+                            break;
+                        case 101:
+                            player.getInventory().setLeggings(item);
+                            break;
+                        case 102:
+                            player.getInventory().setChestplate(item);
+                            break;
+                        case 103:
+                            player.getInventory().setHelmet(item);
+                            break;
+                    }
+                } else {
+                    player.getInventory().setItem(slot, item);
+                }
+            }
+            if (mode.equalsIgnoreCase("add")) {
+                Integer emptySlot = player.getInventory().firstEmpty();
+                player.getInventory().setItem(slot, item);
+            }
+            player.sendMessage("" + item + "\n" + item.getItemMeta());
         }
-
-        upgradeList.get(2).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(3).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(6).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(7).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(10).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(11).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(14).addEnchantment(Enchantment.DAMAGE_ALL, 1);
-        upgradeList.get(15).addEnchantment(Enchantment.DAMAGE_ALL, 1);
     }
 }
